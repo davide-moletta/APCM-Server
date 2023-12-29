7ebcb4bbf9f3a72a60161c125b47b5bd59a43200
Edoardo Mich <71270665+edoardomich@users.noreply.github.com>
2023-12-29 22:56:18 +0100

    Add logger to Guard classes

--- src/main/java/it/unitn/APCM/ACME/Guard/Guard_Connection.java
+++ src/main/java/it/unitn/APCM/ACME/Guard/Guard_Connection.java
@@ -1,13 +1,17 @@
 package it.unitn.APCM.ACME.Guard;
 
 import it.unitn.APCM.ACME.ServerCommon.JDBC_Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 
 public class Guard_Connection {
 	private static final JDBC_Connection dbconn = new JDBC_Connection("db_users.sqlite");
+	private static final Logger log = LoggerFactory.getLogger(Guard_Connection.class);
 
 	public static Connection getDbconn() {
+		log.trace("Connection to db_users requested");
 		return dbconn.getConn();
 	}
 }
--- src/main/java/it/unitn/APCM/ACME/Guard/Guard_RESTApp.java
+++ src/main/java/it/unitn/APCM/ACME/Guard/Guard_RESTApp.java
@@ -1,5 +1,7 @@
 package it.unitn.APCM.ACME.Guard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import java.util.HashMap;
 
 @SpringBootApplication
 public class Guard_RESTApp {
+	private static final Logger log = LoggerFactory.getLogger(Guard_RESTApp.class);
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Guard_RESTApp.class);
 		HashMap<String, Object> properties = new HashMap<String, Object>(2);
@@ -14,5 +17,6 @@ public class Guard_RESTApp {
 		properties.put("spring.application.name", "Guard_RESTApp");
 		app.setDefaultProperties(properties);
 		app.run(args);
+		log.info("Guard_RESTApp started");
 	}
 }
