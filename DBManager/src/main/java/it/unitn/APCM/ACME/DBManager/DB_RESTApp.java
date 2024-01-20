3082ef1e369e857cbc948754df1c852f99a31982
Edoardo Mich <71270665+edoardomich@users.noreply.github.com>
2024-01-20 18:07:14 +0100

    Remove properties from args

--- DBManager/src/main/java/it/unitn/APCM/ACME/DBManager/DB_RESTApp.java
+++ DBManager/src/main/java/it/unitn/APCM/ACME/DBManager/DB_RESTApp.java
@@ -8,6 +8,7 @@ import it.unitn.APCM.ACME.DBManager.SSS.Shamir;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.crypto.SecretKey;
 
@@ -18,33 +19,21 @@ public class DB_RESTApp {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DB_RESTApp.class);
-		String srv_port = "8091";
-		String srv_app = "DBManager_RESTApp";
 		String sss_path = "SSS.txt";
 		switch (args.length) {
 			case 0:
 				// Use default values
 				break;
 			case 1:
-				srv_port = args[0];
-				break;
-			case 2:
-				srv_port = args[0];
-				srv_app = args[1];
-				break;
-			case 3:
-				srv_port = args[0];
-				srv_app = args[1];
-				sss_path = args[2];
+				sss_path = args[0];
 				break;
 			default:
 				log.error("Error in parameters");
-				throw new IllegalArgumentException("At most 3 parameter could be passed");
+				throw new IllegalArgumentException("At most 1 parameter could be passed");
 		}
 		Shamir sh = new Shamir();
 		try {
 			masterKey = sh.getMasterSecret(sss_path);
-			app.setDefaultProperties(Map.of("server.port", srv_port, "spring.application.name", srv_app));
 			app.run(args);
 			log.info("DB_RESTApp started");
 		}
--- Guard/src/main/java/it/unitn/APCM/ACME/Guard/Guard_RESTApp.java
+++ Guard/src/main/java/it/unitn/APCM/ACME/Guard/Guard_RESTApp.java
@@ -13,25 +13,6 @@ public class Guard_RESTApp {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Guard_RESTApp.class);
-		String srv_port = "8090";
-		String srv_app = "Guard_RESTApp";
-		switch (args.length) {
-			case 0:
-				// Use default values
-				break;
-			case 1:
-				srv_port = args[0];
-				break;
-			case 2:
-				srv_port = args[0];
-				srv_app = args[1];
-				break;
-			default:
-				log.error("Error in parameters");
-				throw new IllegalArgumentException("At most 2 parameter could be passed");
-		}
-
-		app.setDefaultProperties(Map.of("server.port", srv_port, "spring.application.name", srv_app));
 		app.run(args);
 		log.info("Guard_RESTApp started");
 	}
